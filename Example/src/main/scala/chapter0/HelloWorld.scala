a11e3cd68a32ce2d34d1a18f9592ccd4d758bb6d
HK Chen <ilikeiceblue@gmail.com>
2016-08-03 15:11:32 +0800

    chapter 0: what's main function in Scala on Eclipse writing

---
+++ Example/src/main/scala/chapter0/HelloWorld.scala
@@ -0,0 +1,8 @@
+package main.scala.chapter0
+
+object HelloWorld {
+    // This def is scala's main function just like java's void main
+    def main(args: Array[String]): Unit={
+        println("Hello, World! This is Scala. :)")
+    }
+}
