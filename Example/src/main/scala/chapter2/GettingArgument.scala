ed28f876f7ceb78aa8a468c4a804519eeb428efb
HK Chen <ilikeiceblue@gmail.com>
2016-08-03 17:13:29 +0800

    modify ch2 GettingArgument

--- Example/src/main/scala/chapter2/GettingArgument.scala
+++ Example/src/main/scala/chapter2/GettingArgument.scala
@@ -5,10 +5,19 @@ object GettingArgument {
         var i = 0
         // How to get args? -> Run Configurations -> 
         // Argument input what you want!
+        
+        // print args 3 ways compare
+        // while loop to print
         while(i < args.length){
             println(args(i))
             i += 1 // i++, i-- not in Scala
         }
-            
+        
+        // foreach to print
+        args.foreach { x => println(x) }
+        
+        // for loop to print 
+        for(x <- args)
+            println(x)
     }
 }
